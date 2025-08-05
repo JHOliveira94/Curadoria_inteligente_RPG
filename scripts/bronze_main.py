5f05631677ec2754bfc5da1d89a6d131620a60a3
José Henrique de Oliveira <jh.oliveira.94@gmail.com>
2025-08-05 01:17:41 -0300

    docs: Docstring bronze_main.py

--- scripts/bronze_main.py
+++ scripts/bronze_main.py
@@ -1,5 +1,18 @@
+"""
+PROJETO: CURADORIA DE SESSÕES DE RPG DE MESA
+
+Main - Execução Principal
+Este arquivo executa as classes definidas em bronze_transcricao.py.
+"""
+
+# ===== IMPORTAÇÕES =====
+# Conecta com bronze_transcricao.py
 
 from bronze_transcricao import RPGTranscricao
+
+
+# ===== FUNÇÃO PRINCIPAL =====
+# Função que coordena e executa os comandos definidos em bronze_transcricao.py.
 
 def main():
     processador = RPGTranscricao()
